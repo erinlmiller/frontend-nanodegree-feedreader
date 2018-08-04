bdc2f9b67431fbb0e820f7444fec1fe85a95f14d
Erin Miller <erinthered324@gmail.com>
2018-08-04 12:16:15 -0400

    Write Initial Entries test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -86,7 +86,9 @@ $(function() {
           });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* DONE. Write a new test suite named "Initial Entries" */
+
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -94,6 +96,7 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+       });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
