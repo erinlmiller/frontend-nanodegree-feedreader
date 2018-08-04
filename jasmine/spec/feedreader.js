48e4519b80ffd405bf43f2b637cd3f8a10dffa84
Erin Miller <erinthered324@gmail.com>
2018-08-04 11:57:59 -0400

    Add test to check menu toggling

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -68,12 +68,22 @@ $(function() {
            expect(body.classList.contains('menu-hidden')).toBe(true);
          });
 
-         /* TODO: Write a test that ensures the menu changes
+         /* DONE. Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
 
+          it('toggles visibility when clicked', function() {
+            const body = document.querySelector('body');
+            const menu = document.querySelector('.menu-icon-link');
+
+            menu.click();
+            expect(body.classList.contains('menu-hidden')).toBe(false);
+
+            menu.click();
+            expect(body.classList.contains('menu-hidden')).toBe(true);
+          });
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
